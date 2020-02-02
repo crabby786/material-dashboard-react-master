96d78227620775af3e18842753edd3533dd0f3eb
crabby786 <39272649+crabby786@users.noreply.github.com>
2020-02-02 00:14:28 -0800

    Updates

---
+++ precache-manifest.fa0f00fd088439b8bee9084fc0944179.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bd873aff40f533e1cad93335e42ec81",
+    "url": "/reactadmin1/index.html"
+  },
+  {
+    "revision": "f95e50384175f5c18f04",
+    "url": "/reactadmin1/static/css/2.d0176e96.chunk.css"
+  },
+  {
+    "revision": "aecc92243c93ce7e9d30",
+    "url": "/reactadmin1/static/css/main.155bc9f5.chunk.css"
+  },
+  {
+    "revision": "f95e50384175f5c18f04",
+    "url": "/reactadmin1/static/js/2.fde9156f.chunk.js"
+  },
+  {
+    "revision": "aecc92243c93ce7e9d30",
+    "url": "/reactadmin1/static/js/main.d8298a70.chunk.js"
+  },
+  {
+    "revision": "53267e39ffd1f098411b",
+    "url": "/reactadmin1/static/js/runtime~main.81cf2e8d.js"
+  },
+  {
+    "revision": "8880a65c57d7f031579335be153f64a0",
+    "url": "/reactadmin1/static/media/marc.8880a65c.jpg"
+  },
+  {
+    "revision": "8e9f0a3c5578a20733d5bad0e51c91fb",
+    "url": "/reactadmin1/static/media/sidebar-1.8e9f0a3c.jpg"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/reactadmin1/static/media/sidebar-2.310509c9.jpg"
+  },
+  {
+    "revision": "2503169017014036cf0dd0dd7c2a2b8a",
+    "url": "/reactadmin1/static/media/sidebar-3.25031690.jpg"
+  },
+  {
+    "revision": "fc9cb0538eb5a4dfd6fbb1bf6dd6189b",
+    "url": "/reactadmin1/static/media/sidebar-4.fc9cb053.jpg"
+  }
+]);
